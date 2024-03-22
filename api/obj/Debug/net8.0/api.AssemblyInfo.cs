585bec58fb1413a40573f9dfeaa1875e1a91bdc0
Vy Mai <aidentmai@gmail.com>
2024-03-22 01:58:33 -0700

    Finished comment delete task

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faca7d1591f1074e558d463985ff3e7f30c78ec6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4904442221e5e8f8a29fc026a97e44023aa8cae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
