a7168b8e030cc7b0bf1d9b117b0cd924b118d132
Vy Mai <aidentmai@gmail.com>
2024-03-22 16:44:00 -0700

    Completed token service

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+800b85a49cc591c864576eab105047ae179b2519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a92f1f0e995666452c5a809bf4071625e6034d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
