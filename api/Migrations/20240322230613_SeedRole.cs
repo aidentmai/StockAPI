e4a92f1f0e995666452c5a809bf4071625e6034d
Vy Mai <aidentmai@gmail.com>
2024-03-22 16:13:42 -0700

    Finished register functionality

---
+++ api/Migrations/20240322230613_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3e7bb9a7-7610-4b69-8024-8884a857cf90", null, "Admin", "ADMIN" },
+                    { "54f55ab6-90cd-4d4f-b817-becc179e156a", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3e7bb9a7-7610-4b69-8024-8884a857cf90");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "54f55ab6-90cd-4d4f-b817-becc179e156a");
+        }
+    }
+}
